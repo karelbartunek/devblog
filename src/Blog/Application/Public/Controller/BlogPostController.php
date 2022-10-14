dfefa53a637d6fd8097f0c95822c9e61910f0f30
karelbartunek <post@karel-bartunek.cz>
2022-10-14 14:33:02 +0200

    Blog - add Blog Post detail

--- src/Blog/Application/Public/Controller/BlogPostController.php
+++ src/Blog/Application/Public/Controller/BlogPostController.php
@@ -2,38 +2,20 @@
 
 namespace App\Blog\Application\Public\Controller;
 
-use App\Blog\Domain\BlogPost\CreateBlogPost;
-use App\Blog\Domain\Entity\BlogPost;
-use App\Blog\Application\Public\Form\BlogPostType;
+use App\Blog\Infrastructure\Repository\BlogPostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogPostController extends AbstractController
 {
-    #[Route(path: 'blog/detail', name: 'blog_detail', methods: ['GET', 'POST'])]
-    public function new(Request $request, CreateBlogPost $createBlogPost): Response
+    #[Route(path: 'blog/detail/{blogPostId}', name: 'blog_detail', methods: ['GET'])]
+    public function detail(int $blogPostId, BlogPostRepository $blogPostRepository): Response
     {
-        $blogPost = new BlogPost();
+        $blogPost = $blogPostRepository->find($blogPostId);
 
-        $form = $this->createForm(BlogPostType::class, $blogPost);
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $blogPost = $form->getData();
-
-            try {
-                $createBlogPost($blogPost);
-            } catch (\Exception) {
-                error_log('Error');
-            }
-
-            return $this->redirectToRoute('blog');
-        }
-
-        return $this->renderForm('blog/blogpost/new.html.twig', [
-            'form' => $form,
+        return $this->render('blog/detail.html.twig', [
+            'blog_post' => $blogPost
         ]);
     }
 }
